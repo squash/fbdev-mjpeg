be6d66d90a2fa441a9599592f321d893995974bb
Your Name <you@example.com>
2023-04-21 09:59:41 -0500

    Pre-fill Alpha channel up front. Make resolution a config option.

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # fbdev-mjpeg
 
 This project is working to implement a remote Linux framebuffer console.
+
+# USAGE
+
+```
+Usage of ./fbdev-mjpeg:
+  -device string
+        Framebuffer device to read (default "/dev/fb0")
+  -height int
+        Framebuffer height in pixels (default 480)
+  -port int
+        Port to service mjpeg stream (default 9876)
+  -width int
+        Framebuffer width in pixels (default 640)
+```
--- main.go
+++ main.go
@@ -21,7 +21,11 @@ type imageBlock struct {
 func main() {
 	fn := flag.String("device", "/dev/fb0", "Framebuffer device to read")
 	port := flag.Int64("port", 9876, "Port to service mjpeg stream")
+	width:=flag.Int("width", 640, "Framebuffer width in pixels")
+	height:=flag.Int("height", 480, "Framebuffer height in pixels")
 	flag.Parse()
+
+	// make sure we can open the framebuffer device before we begin
 	file, err := os.Open(*fn)
 	if err != nil {
 		log.Fatalf("Couldn't open console device: %s\n", err.Error())
@@ -29,7 +33,7 @@ func main() {
 	file.Close()
 
 	c := make(chan imageBlock, 1)
-	go fbLoop(*fn, c)
+	go fbLoop(*fn, *width, *height, c)
 	o := &jpeg.Options{Quality: 75}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "multipart/x-mixed-replace; boundary=frame")
@@ -59,12 +63,17 @@ func main() {
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
 
-func fbLoop(file string, c chan imageBlock) {
-	res := 640 * 480
-	t := make([]byte, res*3)
-	rgba := make([]byte, res*4)
+func fbLoop(file string, width, height int, c chan imageBlock) {
+	pixels:=width*height
+	t := make([]byte, pixels*3) // 3 bytes per pixel RGB
+	rgba := make([]byte, pixels*4) // 4 bytes per pixel RGBA
 	var i imageBlock
-	i.i = image.NewRGBA(image.Rect(0, 0, 640, 480))
+
+	i.i = image.NewRGBA(image.Rect(0, 0, width, height))
+	// Smart! Pre-fill the Alpha channel bit
+	for x:=0;x<pixels;x++ {
+		i.i.Pix[(x*4)+3]=255
+	}
 	var l [16]byte
 	for {
 		f, err := os.Open(file)
@@ -84,14 +93,13 @@ func fbLoop(file string, c chan imageBlock) {
 		if err != nil {
 			log.Fatalf("Read error: %s", err.Error())
 		}
-		if n != res*3 {
+		if n != pixels*3 {
 			log.Fatal("Short read")
 		}
-		for x := 0; x < (640 * 480); x++ {
+		for x := 0; x < pixels; x++ {
 			rgba[x*4] = t[x*3]
 			rgba[(x*4)+1] = t[(x*3)+1]
 			rgba[(x*4)+2] = t[(x*3)+2]
-			rgba[(x*4)+3] = 255
 		}
 		i.i.Pix = rgba
 		c <- i
